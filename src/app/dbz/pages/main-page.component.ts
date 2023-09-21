f63961a8cbac0b8782766abf74411634a87e0966
rcaac <aatachaguacontreras@gmail.com>
2023-09-21 10:22:15 -0500

    add module dbz

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,24 @@
+import { Component} from '@angular/core';
+import { Character } from './../interfaces/character.interface';
+
+
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent {
+
+  public characters: Character[] = [{
+    name: 'krillin',
+    power: 1000
+  },{
+    name: 'goku',
+    power: 9500,
+  },{
+    name: 'vegeta',
+    power: 7500,
+  },
+]
+}
